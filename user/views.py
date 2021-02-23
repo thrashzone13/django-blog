77921547b59a479fa80702a07123dd702904740c
thrashzone13 <reza.rabbani.369@gmail.com>
2021-02-23 23:45:55 +0330

    gitignore added

--- user/views.py
+++ user/views.py
@@ -1,24 +1,26 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from pprint import pprint
+
 # Create your views here.
 
 
 def register(request):
     if request.method == 'POST':
+        pprint(dir(request.POST))
         form = UserCreationForm(request.POST)
         if form.is_valid():
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'User: {username} created successfully')
+            form.save()
+            messages.success(request, f'Success! Now sign in')
             return redirect('login')
-        else:
-            messages.warning(request, list(form.error_messages.values())[0])
-            return redirect('register')
     else:
-        return render(request, 'user/register.html', {
-            'form': UserCreationForm(),
-            'title': "Sign up"
-        })
+        form = UserCreationForm()
+
+    return render(request, 'user/register.html', {
+        'form': form,
+        'title': "Sign up"
+    })
 
 
 def login(request):
